1b1256ee9b8e5bcc5ef473e1c570719bbfb0cf53
MariaStechenko <121880216+MariaStechenko@users.noreply.github.com>
2023-02-08 23:18:11 +0200

    Update Article.jsx

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,28 +1,19 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Menu from './Menu';
-import { db } from '../firebase';
-import { query, onSnapshot, collection } from 'firebase/firestore';
-
-//import articles from '../data/articles';
-
-const q = query(collection(db, 'articlelist-1e55a'));
+import ArticleDataService from '../services/article.services';
 
 function Article() {
   const [articles, setArticles] = useState([]);
-
   useEffect(() => {
-    onSnapshot(q, (snapshot) => {
-      setArticles(
-        snapshot.doc.map((doc) => ({
-          id: doc.id,
-          title: doc.title(),
-          description: doc.description(),
-          img: doc.imageUrl,
-        }))
-      );
-    });
+    getArticles();
   }, []);
+
+  const getArticles = async () => {
+    const data = await ArticleDataService.getAllArticles();
+    console.log(data.docs);
+    setArticles(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  };
 
   return (
     <>
@@ -32,7 +23,7 @@ function Article() {
         <div className="ArticleItem">
           {articles.map((article) => (
             <div key={article.id} className="ArticleContainer">
-              <Link to={article.id.toString()} className="ArticleLink">
+              <Link to={article.id} className="ArticleLink">
                 <img
                   src={article.imageUrl}
                   alt="Generic placeholder"
